e338c5bdca7b4fd073e65bafbbc58785b9f1e694
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2023-10-31 15:41:25 +0100

    exact.jl: Fix frequency calculation using transform(..) and simplifying the code, as well as update CI

--- src/exact.jl
+++ src/exact.jl
@@ -4,24 +4,13 @@ module exact
     using ProgressMeter
     using Folds
 
-    function compute_and_add_frequency(df::DataFrame)
-        gdf = groupby(df, [:case, :gene])
-    
-        function compute_frequency(sub_df)
-            gene_total_count = sum(sub_df[:, :count])
-            frequency = sub_df[:, :count] ./ gene_total_count
-            return DataFrame(frequency=frequency)
-        end
-        df[:,:frequency] .= combine(compute_frequency, gdf)[:,:frequency]
-        return df
-    end
-
     """
         exact_search(table, query)
 
     Exact search per case
     """
     function exact_search(table, query; mincount=10, minfreq=0.2)
+        @info "Using mincount: $mincount and minfreq: $minfreq"
         @assert all(names(table) .== ["case","name","genomic_sequence"]) "File must contain following columns case,name,genomic_sequence"
         p = Progress(nrow(table))
         result = Folds.map(eachrow(table)) do row
@@ -32,7 +21,8 @@ module exact
                 match = occursin(seq, row.genomic_sequence)
                 if match
                     push!(matches, (string(case), string(name)))
-                    break
+                    # Comment, because we cannot guarantee thet sequences in database are unique
+                    # break
                 end
             end
             matches
@@ -41,7 +31,8 @@ module exact
         rename!(result_df, [:case, :db_name])
         final = combine(groupby(result_df,[:case,:db_name]),nrow => :count)
         final[:,:gene] = map(x->first(split(x.db_name,'*')),eachrow(final))
-        final = compute_and_add_frequency(final)
-        filter(r->(r.count >= mincount) & (r.frequency >= minfreq) , final)       
+        gdf = groupby(final, [:case, :gene])
+        final = transform(gdf, :count => (x->x./maximum(x))=> :frequency)
+        filter(r->(r.count >= mincount) & (r.frequency >= minfreq) , final)
     end
 end
